024f52411af4f3c07d636d4efc1a140abaa05250
ming <ming@163.com>
2019-06-30 18:18:18 +0800

    fifth提交(websocket实例)

---
+++ src/main/java/com/example/netty/fifth/MyServerInitializer5.java
@@ -0,0 +1,27 @@
+package com.example.netty.fifth;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+/**
+ * @author hzq
+ * @date 2019/6/30 0030 下午 5:48
+ */
+public class MyServerInitializer5 extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+
+        pipeline.addLast(new HttpServerCodec());
+        pipeline.addLast(new ChunkedWriteHandler());
+        pipeline.addLast(new HttpObjectAggregator(8192));
+        pipeline.addLast(new WebSocketServerProtocolHandler("/ws"));
+        pipeline.addLast(new TextWebSockerFrameHandler());
+    }
+}
